2cb4fa5c1fcbdc752fc6977d06418a708c500b3f
shrutidhungana <shrutidhungana@prixa.net>
2022-08-11 11:42:40 +0545

    NavLink completed

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Nav
+export default Nav;
