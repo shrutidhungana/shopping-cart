8b98304fb915417071dcb32ca1d6870aacb257f4
shrutidhungana <shrutidhungana@prixa.net>
2022-08-11 20:02:53 +0545

    Done with adding

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useContext} from "react";
+import { CartContext } from "../../Context/CartContext";
+import './Cart.css'
 
 const Cart = () => {
+  
+  const { cartItems } = useContext(CartContext);
+  console.log(cartItems);
   return (
-    <div>Cart</div>
-  )
-}
+    <div >
+      
+    { cartItems.length&&<div className="loading">
+    There are no items in Cart
+    </div>}
+      {cartItems.map((cartItem, index) => (
+        <div className = "Cart-item-wrapper"key = {cartItem.id}>
+          <div>
+            <img className = "Cart-img" src={cartItem.image} alt="Cart" />
+          </div>
+          <div>{cartItem.id}</div>
+          <div>
+            <p className="Cart-title">{cartItem.title}</p>
+          </div>
+          <div>
+            <p>{cartItem.price}</p>
+          </div>
+          <div className = "Cart-quantity-wrapper">
+            <button>-</button>
+            <p>{cartItem.quantity}</p>
+            <button>+</button>
+          </div>
+          <div>
+            <button className = "Cart-remove-button">Remove from Cart</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
--- src/Reducer/CartReducer.jsx
+++ src/Reducer/CartReducer.jsx
@@ -3,8 +3,33 @@ import { ADD_TO_CART, REMOVE_FROM_CART } from "./ReducerTypes";
 const CartReducer = (state, action) => {
   switch (action.type) {
     case ADD_TO_CART:
-      const { cartItem } = state;
+      const { cartItems } = state;
       const { currentProductItem } = action;
+      let updateCartItems = [...cartItems]
+      console.log(updateCartItems, state);
+      
+      const checkIndexofCurrentItem = updateCartItems.findIndex(item => item.id === currentProductItem.id);
+      if (checkIndexofCurrentItem === -1) {
+        updateCartItems.push({
+          ...currentProductItem,
+          quantity : 1
+        })
+      } else {
+        const checkIfTheItemIsAlreadyExists = updateCartItems.find(item => item.id === updateCartItems[checkIndexofCurrentItem].id)
+        console.log(checkIfTheItemIsAlreadyExists);
+        updateCartItems[checkIndexofCurrentItem] = {
+          ...checkIfTheItemIsAlreadyExists,
+          quantity: checkIfTheItemIsAlreadyExists.quantity + 1
+        }
+
+        console.log(updateCartItems);
+      }
+
+      return {
+        ...state,
+        cartItems: updateCartItems
+      }
+     
       
     case REMOVE_FROM_CART:
       console.log("Remove from cart is called");
