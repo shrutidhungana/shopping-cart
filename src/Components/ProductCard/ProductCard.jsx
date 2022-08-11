c223516d3ea725242d1c23313936470294900a9d
shrutidhungana <shrutidhungana@prixa.net>
2022-08-11 16:19:13 +0545

    Made the product in card

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import './ProductCard.css'
+import React from "react";
+import "./ProductCard.css";
 
 const ProductCard = (props) => {
-
   const { product } = props;
   console.log(product);
   const { category, id, price, title, rating, image } = product;
   return (
-    <div className = "ProductCard-wrapper">
-      <img className = "ProductCard-image" src={image} alt="Product" />
-      <p className = "ProductCard-id">{id }</p>
-      <p className = "ProductCard-category">{category }</p>
-      <p className = "ProductCard-title">{title }</p>
-      <p className = "ProductCard-price"> Rs.{price }</p>
-      <p className = "ProductCard-rating">{ rating.rate}</p>
-      <button className = "ProductCard-button">Add to cart</button>
+    <div className="ProductCard-wrapper">
+      <div className="ProductCard-image">
+        <img src={image} alt="Product" />
+      </div>
+      <p className="ProductCard-id">{id}</p>
+      <p className="ProductCard-category">{category}</p>
+      <p className="ProductCard-title">{title}</p>
+      <p className="ProductCard-price"> Rs.{price}</p>
+      <p className="ProductCard-rating">{rating.rate}</p>
+      <button className="ProductCard-button">Add to cart</button>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
