0cd26bed87b2cca796cdd4387fbbd292de4c6a3b
shrutidhungana <shrutidhungana@prixa.net>
2022-10-20 13:23:47 +0545

    Hosted

--- src/AppRouter/AppRouter.jsx
+++ src/AppRouter/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "../Components/Nav/Nav";
 import ProductList from "../Components/ProductList/ProductList"
 import Cart from "../Components/Cart/Cart"
